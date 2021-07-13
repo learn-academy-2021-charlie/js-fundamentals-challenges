20bc2debde9fbd8acc2b68f1008ec6abe0fac45c
Bpompa95 <brianpompa02@gmail.com>
2021-07-13 16:33:58 -0700

    finish

--- high-order-function-todd-brian.js
+++ high-order-function-todd-brian.js
@@ -4,10 +4,20 @@
 // what Parameter (array)
 // Use multiplied by 10
 // Use .map include a return value
-var arr1 = [3, 9, 15, 4, 10]
+// var arr1 = [3, 9, 15, 4, 10]
 
-const loki = arr1.map(value => {
-    return value * 10
-})
+// const loki = arr1.map(value => {
+//     return value * 10
+// })
 
-console.log(loki)
+// console.log(loki)
+
+// var arr2 = [2, 7, 3, 5, 8, 10, 13] 
+
+// const onlyOdds = (array) => {
+//     return array.filter(value => value % 2 !== 0)
+//   }
+  
+//   console.log(onlyOdds(arr2))
+
+var comboArr = [7, "n", "i", "c", 10, "e", false, "w", 3, "o", "r", "k"]
