16731af423345306f50189d74ec6711bb1e50f10
charper95 <charper1995@gmail.com>
2021-07-13 15:24:48 -0700

    initial commit

---
+++ js-higher-order-conrad-amanda.js
@@ -0,0 +1,41 @@
+// Write a function that takes in an array and returns a new array with all numbers multiplied by 10.
+
+// var arr1 = [3, 9, 15, 4, 10]
+
+// // expected output [30, 90, 150, 40, 100]
+
+// // Create a function called mult10
+// // Parameter - array
+// // Iterate
+// // Return an array of the same length
+// // use map
+// // multiply each number by 10
+// const mult10 = (array) => {
+//     return array.map(value =>{
+//         return value * 10
+//     })
+// }
+
+// console.log(mult10(arr1))
+
+// Write a function that takes in an array and returns a new array with only odd numbers
+
+// var arr2 = [2, 7, 3, 5, 8, 10, 13]
+
+// // Expected output [7, 3, 5, 13]
+
+// // Create Function called oddNum
+// // Parameter - array
+// // Return an array with only odd numbers
+// // use map
+
+// const oddNum = (array) => {
+//     let oddArray = array.filter(value => {
+//         if (value % 2 !== 0) {
+//             return value
+//         }
+//     })
+//     return oddArray
+// }
+// console.log(oddNum(arr2))
+
