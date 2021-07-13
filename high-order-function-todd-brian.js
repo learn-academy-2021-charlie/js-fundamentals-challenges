c65c63ad681ae9c3ca6efdb583c5a04e257b9e33
ToddComte <comtetodd@gmail.com>
2021-07-13 16:59:21 -0600

    complteted one

---
+++ high-order-function-todd-brian.js
@@ -0,0 +1,13 @@
+// Write a function that takes in an array and returns a new array with all numbers multiplied by 10.
+
+// name the function (loki)
+// what Parameter (array)
+// Use multiplied by 10
+// Use .map include a return value
+var arr1 = [3, 9, 15, 4, 10]
+
+const loki = arr1.map(value => {
+    return value * 10
+})
+
+console.log(loki)
