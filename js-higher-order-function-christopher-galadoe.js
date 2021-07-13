c24e3239f704e8d1a12e6289805aaf33a2e08908
galadoe <kgaladoe@gmail.com>
2021-07-13 15:20:25 -0700

    questions 1-3

---
+++ js-higher-order-function-christopher-galadoe.js
@@ -0,0 +1,28 @@
+//Higher-Order-Function
+
+//1.
+var arr1 = [3, 9, 15, 4, 10]
+const multply10 = (array) => {
+  return array.map(value => {
+    return value * 10
+  })
+}
+console.log(multply10(arr1));
+
+//2.
+var arr2 = [2, 7, 3, 5, 8, 10, 13]
+const oddNum = (array) => {
+  return array.filter(value => {
+    return value % 2 !== 0
+  })
+}
+console.log(oddNum(arr2));
+
+//3.
+var comboArr = [7, "n", "i", "c", 10, "e", false, "w", 3, "o", "r", "k"]
+const letter = (array) => {
+  return array.filter(value => {
+    return typeof value === "string"
+  })
+}
+console.log(letter(comboArr).join(""));
