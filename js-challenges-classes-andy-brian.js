f4cb544119cdbb3d8e36176e617e5dab7222ce18
Andy19936 <andy19936@gmail.com>
2021-07-15 11:30:31 -0700

    switch

--- js-challenges-classes-andy-brian.js
+++ js-challenges-classes-andy-brian.js
@@ -33,8 +33,14 @@ var blackCoffee = new Coffee("black",0 ,0 )
 // Write the code that makes a black coffee.
 console.log(blackCoffee)
 // Write the code that makes a coffee with 1 cream and 2 sugars.
-var CreamSugars = new Coffee("1 cream 2 sugars",1, 2)
+var creamSugars = new Coffee("1 cream 2 sugars",1, 2)
+console.log(creamSugars)
 // Write the code that makes a coffee with 2 sugars. Then write the code that outputs the coffee's profile.
+var twoSugars = new Coffee("Black and Sweet",0,2)
+console.log(twoSugars)
+
+
+
 
 // Latte Maker
 // Write a Latte class that receives a flavor, a milk type and a number of shots.
@@ -45,6 +51,34 @@ var CreamSugars = new Coffee("1 cream 2 sugars",1, 2)
 
 // Write the code that makes a double shot hazelnut latte with almond milk. Then, log the latte's profile.
 
+class Latte {
+
+  constructor(flavor, milktype, shots){
+    this.flavor = flavor.toLowerCase()
+    this.milktype = milktype
+    this.shots = shots
+  }
+  coffeeProfile(){
+    return(`${this.flavor}: ${this.milktype()}, ${this.shots()}`)
+  }
+
+  creams(){
+    if (this.cream > 1){
+      return `${this.cream} creams`
+    } else {
+      return `${this.cream} cream`
+    }
+  }
+
+  sugars(){
+    if (this.sugar > 1){
+      return `${this.sugar} sugars`
+    } else {
+      return `${this.sugar} sugar`
+    }
+    
+    }   
+}
 // Volume of a Cylinder
 // Write a class that calculates the volume of a Cylinder to four decimal places. Volume of a cylinder : V = πr2h (r is the radius and h is the height of the cylinder)
 
