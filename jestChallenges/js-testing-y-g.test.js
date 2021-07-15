911eced5c04628d41e4b5484c64bcff4c3fd6dde
kel-man <kyyafuso@gmail.com>
2021-07-15 09:26:40 -0700

    finishing up challenges for merge

--- jestChallenges/js-testing-y-g.test.js
+++ jestChallenges/js-testing-y-g.test.js
@@ -128,15 +128,67 @@ describe('rick', () => {
 // Write the function that will make the test pass.
 const rick = () => 'Morty'
 // Write the test for a function called greeter that takes a name as an argument and returns a greeting with that name to the screen.
+describe('greeting', () => {
+  it('returns a greeting with the name', () => {
+    expect(greeting('Kelen')).toEqual('Greetings, Kelen!')
+  })
+})
 
 // Write the function that will make the test pass.
+const greeting = (str) => {
+  return `Greetings, ${str}!`
+}
 // Write the test for a function called oddOrEven that takes a number as an argument and logs whether the number is odd or even.
+describe('oddOrEven', () => {
+  it('logs odd for odd numbers', () => {
+    expect(oddOrEven(9)).toEqual('Odd')
+  })
+  it('logs even for even numbers', () => {
+    expect(oddOrEven(4)).toEqual('Even')
+  })
+})
 // Write the function that will make the test pass.
+const oddOrEven = (int) => {
+  return int%2==0 ? 'Even' : 'Odd'
+}
 // Write the test for a function called doubler that takes a number and returns the result of the number multiplied by 2.
+describe('doubler', () => {
+  it('doubles the number input', () => {
+    expect(doubler(10)).toEqual(20)
+  })
+})
 // Write the function that will make the test pass.
+const doubler = (int) => {
+  return int*2
+}
 // Write the test for a function called multiply that takes two numbers as arguments and logs the result of one of the numbers multiplied by the other.
+//
+describe('multiply', () => {
+  it('multiplies two numbers', () => {
+    expect(multiply(5, 10)).toEqual(50)
+  })
+})
 // Write the function that will make the test pass.
+const multiply = (num1, num2) => {
+  return num1*num2
+}
 // Write the test for a function called divisibleBy that takes two numbers as arguments and returns whether the first number is evenly divisible by the second so that divisibleBy(10, 5) logs "10 is evenly divisible by 5".
+describe('divisibleBy', () => {
+  it('tells whether the first number is divisible by the second', () => {
+    expect(divisibleBy(10, 5)).toEqual('yes')
+  })
+  it('tells if the first number is not divisible by second', () => {
+    expect(divisibleBy(10, 3)).toEqual('no')
+  })
+})
 // Write the function that will make the test pass.
+const divisibleBy = (num1, num2) => {
+  return num1%num2==0 ? 'yes' : 'no'
+}
 // Write the test for a function called fizzbuzz. If a number is a multiple of 3, replace it with the word 'fizz'. If a number is a multiple of five, replace it with the word 'buzz'. If a number is a multiple of both 3 and 5, replace it with 'fizzbuzz'.
+// describe('fizzbuzz', () => {
+//   it('replaces multiples with strings', () => {
+//     expect(
+//   })
+// })
 // Write the function that will make the test pass.
